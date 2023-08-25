ac8aad6d5799d4d3745faeb83925a3e81e352509
Washington Almeida <washingtonjaaj@gmail.com>
2023-08-25 11:55:48 -0300

    Delete convertendo-ano-mes-dia-em-data.sql

--- convertendo-ano-mes-dia-em-data.sql
+++
@@ -1,8 +0,0 @@
-/****** Script do comando SelectTopNRows de SSMS  ******/
-SELECT 
-      [FullDateAlternateKey]
-      ,[DayNumberOfMonth]
-      ,[MonthNumberOfYear]
-      ,[CalendarYear]
-	  ,cast(CONCAT(CalendarYear,'-',MonthNumberOfYear,'-',DayNumberOfMonth) as date) as Data
-  FROM [AdventureWorksDW2022].[dbo].[DimDate]
